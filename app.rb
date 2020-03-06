5d48b1c9d047fd0ad7e6bdbb6845c22a91c4bb88
kopo4eweb <kopo4e.web@yandex.ru>
2020-03-06 11:18:20 +0300

    save form visit to db

--- app.rb
+++ app.rb
@@ -15,6 +15,10 @@ end
 class Barber < ActiveRecord::Base
 end
 
+def output_error validate, params
+  validate.select {|key,_| params[key] == ''}.values.join(", ")
+end
+
 get '/' do
   @barbers = Barber.all
   erb :index
@@ -23,4 +27,36 @@ end
 get '/visit' do
   @barbers = Barber.all
   erb :visit
+end
+
+post '/visit' do
+
+  @error = nil
+
+  validate = {
+    :name => "Введите имя",
+    :phone => "Введите телефон",
+    :datestamp => "Введите дату и время"
+  }
+
+  @barbers = Barber.all
+
+  @name = params[:name]
+  @phone = params[:phone]
+  @datestamp = params[:datestamp]
+  @barber = params[:barber]
+  @color = params[:color]
+
+  @error = output_error(validate, params)
+
+  if !@error.empty?
+    return erb :visit
+  end
+
+    @error = nil
+
+    Client.create(:name => @name, :phone => @phone, :datestamp => @datestamp,  :barber => @barber, :color => @color)
+
+    erb "Мастер #{@barber} ждем вас к #{@datestamp}."
+
 end
